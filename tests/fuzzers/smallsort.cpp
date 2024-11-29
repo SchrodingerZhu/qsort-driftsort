e1e1f30df1f33dfdbbe033aaa3cdc1ee2336300c
Schrodinger ZHU Yifan <i@zhuyi.fan>
2024-11-28 21:14:09 -0500

    [smallsort] change to blob implementation

--- tests/fuzzers/smallsort.cpp
+++ tests/fuzzers/smallsort.cpp
@@ -5,37 +5,24 @@
  */
 
 #include "driftsort/smallsort.h"
+#include "../utils.hpp"
 #include <algorithm>
 #include <array>
 #include <fuzztest/fuzztest.h>
 #include <gtest/gtest.h>
-
-#include "../utils.hpp"
 
 using namespace driftsort;
 using namespace driftsort::small;
 
 void sort4_stable_int(std::array<int, 4> in) {
   std::array<int, 4> dst;
-  sort4_stable(in.begin(), dst.begin(), std::less<>());
+  sort4_stable(in.begin(), dst.begin(), compare_blob<int>);
   ASSERT_TRUE(std::is_sorted(dst.begin(), dst.end()));
 }
 
 void sort4_stable_int_reverse(std::array<int, 4> in) {
   std::array<int, 4> dst;
-  sort4_stable(in.begin(), dst.begin(), std::greater<>());
-  ASSERT_TRUE(std::is_sorted(dst.begin(), dst.end(), std::greater<>()));
-}
-
-void sort4_stable_string(std::array<std::string, 4> in) {
-  std::array<std::string, 4> dst;
-  sort4_stable(in.begin(), dst.begin(), std::less<>());
-  ASSERT_TRUE(std::is_sorted(dst.begin(), dst.end()));
-}
-
-void sort4_stable_string_reverse(std::array<std::string, 4> in) {
-  std::array<std::string, 4> dst;
-  sort4_stable(in.begin(), dst.begin(), std::greater<>());
+  sort4_stable(in.begin(), dst.begin(), compare_blob<int, std::greater<>>);
   ASSERT_TRUE(std::is_sorted(dst.begin(), dst.end(), std::greater<>()));
 }
 
@@ -46,7 +33,7 @@ void sort4_is_stable(std::array<int, 4> in) {
   std::array<ElementWithSrc<int>, 4> dst;
   for (auto &e : src)
     e.record_address();
-  sort4_stable(src.begin(), dst.begin(), std::less<>());
+  sort4_stable(src.begin(), dst.begin(), compare_blob<int, std::less<>>);
   for (int i = 0; i < 4; i++) {
     for (int j = 0; j < 4; j++) {
       auto x = dst[i];
@@ -59,6 +46,4 @@ void sort4_is_stable(std::array<int, 4> in) {
 
 FUZZ_TEST(DriftSortTest, sort4_stable_int);
 FUZZ_TEST(DriftSortTest, sort4_stable_int_reverse);
-FUZZ_TEST(DriftSortTest, sort4_stable_string);
-FUZZ_TEST(DriftSortTest, sort4_stable_string_reverse);
 FUZZ_TEST(DriftSortTest, sort4_is_stable);
