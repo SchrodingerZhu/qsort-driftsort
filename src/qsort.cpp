53dbc61abebaa9bbc7b6e955175a6127f538d8e7
Schrodinger ZHU Yifan <i@zhuyi.fan>
2024-11-30 19:56:28 -0500

    [qsort] fix build

--- src/qsort.cpp
+++ src/qsort.cpp
@@ -2,8 +2,11 @@
 #include "driftsort/driftsort.h"
 #include <cstdlib>
 
+typedef int (*compar_d_fn_t)(const void *, const void *, void *);
+typedef int (*compar_fn_t)(const void *, const void *);
+
 extern "C" void qsort_r(void *base, size_t nmemb, size_t size,
-                        __compar_d_fn_t compar, void *arg) {
+                        compar_d_fn_t compar, void *arg) {
   driftsort::qsort_r(base, nmemb, size,
                      [compar, arg](const void *a, const void *b) {
                        return compar(a, b, arg);
@@ -11,6 +14,6 @@ extern "C" void qsort_r(void *base, size_t nmemb, size_t size,
 }
 
 extern "C" void qsort(void *base, size_t nmemb, size_t size,
-                      __compar_fn_t compar) {
+                      compar_fn_t compar) {
   driftsort::qsort_r(base, nmemb, size, compar);
 }
