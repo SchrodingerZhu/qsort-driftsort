1d303b0cb9d80e5e5f613537251b72c5c75b51c3
Schrodinger ZHU Yifan <yifanzhu@rochester.edu>
2024-12-01 14:56:50 -0500

    [ci] adjust for apple

--- src/qsort.cpp
+++ src/qsort.cpp
@@ -1,8 +1,19 @@
 
 #include "driftsort/driftsort.h"
+
+#ifdef __APPLE__
+typedef int (*compar_d_fn_t)(void *, const void *, const void *);
+typedef int (*compar_fn_t)(const void *, const void *);
+extern "C" void qsort_r(void *base, size_t nmemb, size_t size, void *arg,
+                        compar_d_fn_t compar) {
+  driftsort::qsort_r(base, nmemb, size,
+                     [compar, arg](const void *a, const void *b) {
+                       return compar(arg, a, b);
+                     });
+}
+#else
 typedef int (*compar_d_fn_t)(const void *, const void *, void *);
 typedef int (*compar_fn_t)(const void *, const void *);
-
 extern "C" void qsort_r(void *base, size_t nmemb, size_t size,
                         compar_d_fn_t compar, void *arg) {
   driftsort::qsort_r(base, nmemb, size,
@@ -10,7 +21,7 @@ extern "C" void qsort_r(void *base, size_t nmemb, size_t size,
                        return compar(a, b, arg);
                      });
 }
-
+#endif
 extern "C" void qsort(void *base, size_t nmemb, size_t size,
                       compar_fn_t compar) {
   driftsort::qsort_r(base, nmemb, size, compar);
